8a8362879d7ac0158cd64f5b04ebb4084f5a92be
Erich Richter <erich.richter@gmail.com>
2020-06-19 11:55:53 -0700

    add menu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,48 @@
 
-function Pizza(toppings){
-this.toppings = toppings
-//this.size = ""
+function Pizza(){
+this.toppings = []
+this.size = ""
 }
 
 Pizza.prototype.cost = function(){
 
 }
 
-
+let pizza = new Pizza
+Pizza.prototype.toppings = function(){
+    
+    this.toppings = [];
+    $('#toppings input[type="checkbox"]:checked').each(function(){
+      this.toppings.push($(this).val());
+    });
+}
+    console.log(pizza.toppings)
 
 $(document).ready(function(){
-    Pizza.prototype.toppings = function(){
-        this.toppings = [];
-        $('#toppings input[type="checkbox"]:checked').each(function(){
-          this.toppings.push($(this).val());
-        });
-       //return toppings
-    }
-
     
+    
+
+
     
     $("#show-order").click(function () {
         let pizza = new Pizza()
-        pizza.toppings()
-        pizza.size = parseInt($("#size").val())
-       // pizza.toppings = []
-        console.log(pizza.size)
+      
+        function dumpInArray(){
+            //var toppings = [];
+            $('#toppings input[type="checkbox"]:checked').each(function(){
+               pizza.toppings.push($(this).val());
+            });
+            //return toppings; 
+         }
+        console.log(dumpInArray())
+        console.log(pizza)
+      
         console.log(pizza.toppings)
+        
+        pizza.size = parseInt($("#size").val())
+       
+        console.log(pizza.size)
+        
     $("#output").html("Your order:" + "<br>" +  $("#size option:selected").text() + "<br>" + pizza.toppings)
     $("#output").show();
   });
