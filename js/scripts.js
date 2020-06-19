ae8373eb66ac1cb0fd38302acfa37163430bf946
Erich Richter <erich.richter@gmail.com>
2020-06-19 10:06:43 -0700

    add js, html, css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+
+function Pizza(){
+this.toppings = []
+//this.size = ""
+}
+
+Pizza.prototype.cost = function(){
+
+}
+
+
+
+
 $(document).ready(function(){
+
+    
+    
+    $("#place-order").click(function () {
+        let pizza = new Pizza()
+        pizza.size = parseInt($("#size").val())
+        pizza.toppings = []
+        console.log(pizza.size)
+    
+    $("#output").text($("#size option:selected").text())
+    $("#output").show();
+  });
+
+
+
+
 
 });
