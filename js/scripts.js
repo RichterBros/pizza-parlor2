9e358ab8f74e2aa5f153f0fe95b620daa2379e77
Erich Richter <erich.richter@gmail.com>
2020-06-19 12:52:12 -0700

    add cost total functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,54 @@
 
 function Pizza(){
 this.toppings = []
-this.size = ""
+this.size = 0
+this.cost = 0
 }
 
-Pizza.prototype.cost = function(){
+Pizza.prototype.price = function(){
+if (this.size === 1){
+    this.cost = 10
+}else if(this.size === 2){
+    this.cost = 15
+}else if (this.size === 3){
+    this.cost = 20
+}
+return this.cost
 
 }
 
 let pizza = new Pizza
-Pizza.prototype.toppings = function(){
+// Pizza.prototype.toppings = function(){
     
-    this.toppings = [];
-    $('#toppings input[type="checkbox"]:checked').each(function(){
-      this.toppings.push($(this).val());
-    });
-}
+//     this.toppings = [];
+//     $('#toppings input[type="checkbox"]:checked').each(function(){
+//       this.toppings.push($(this).val());
+//     });
+// }
     console.log(pizza.toppings)
 
 $(document).ready(function(){
     
     
 
+   
+    let pizza = new Pizza()
 
     
     $("#show-order").click(function () {
-        let pizza = new Pizza()
-      
-        function dumpInArray(){
-            //var toppings = [];
-            $('#toppings input[type="checkbox"]:checked').each(function(){
-               pizza.toppings.push($(this).val());
-            });
-            //return toppings; 
-         }
-        console.log(dumpInArray())
-        console.log(pizza)
-      
-        console.log(pizza.toppings)
-        
-        pizza.size = parseInt($("#size").val())
+        $('#toppings input[type="checkbox"]:checked').each(function(){
+            pizza.toppings.push($(this).val());
+          });
+   pizza.price(pizza)
        
-        console.log(pizza.size)
-        
+       console.log(pizza.toppings)
+       
+       pizza.size = parseInt($("#size").val())
+       
+       console.log(pizza.size)
+       console.log(pizza)
+       console.log(pizza.price(pizza))
     $("#output").html("Your order:" + "<br>" +  $("#size option:selected").text() + "<br>" + pizza.toppings)
     $("#output").show();
   });
-
-
-
-
-
 });
