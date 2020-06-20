b95eb0ad7954d348690932ac2d16215a2d3a3301
Erich Richter <erich.richter@gmail.com>
2020-06-19 17:04:08 -0700

    add styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 
-function Pizza(){
-this.toppings = []
-this.size = 0
-this.cost = 0
+function Pizza() {
+    this.toppings = []
+    this.size = 0
+    this.cost = 0
 }
 
-Pizza.prototype.price = function(){
+Pizza.prototype.price = function () {
 
-for (i = 0; i < this.toppings.length ; i++){
+    for (i = 0; i < this.toppings.length; i++) {
 
-    this.cost += 2
-}
+        this.cost += 2
+    }
 
 
 
-    if (this.size === 1){
-    this.cost += 10
-}else if(this.size === 2){
-    this.cost += 15
-}else if (this.size === 3){
-    this.cost += 20
-}
-return this.cost
+    if (this.size === 1) {
+        this.cost += 10
+    } else if (this.size === 2) {
+        this.cost += 15
+    } else if (this.size === 3) {
+        this.cost += 20
+    }
+    return this.cost
 
 }
 
@@ -29,35 +29,39 @@ let pizza = new Pizza
 
 console.log(pizza.toppings)
 
-$(document).ready(function(){
-    
-    
+$(document).ready(function () {
 
-   
+
+
+
     let pizza = new Pizza()
-    
-  
-    
+
+
+
     $("#show-order").click(function () {
-        $('#toppings input[type="checkbox"]:checked').each(function(){
+        $('#toppings input[type="checkbox"]:checked').each(function () {
             pizza.toppings.push("<li>" + $(this).val());
-          });
-  
-      
-       console.log(pizza.toppings)
-       
-       pizza.size = parseInt($("#size").val())
-       
-       console.log(pizza.size)
-       console.log(pizza.price(pizza))
-       console.log(pizza)
-    $("#output").html("<h4>Your order:</h4>" +  "<hr class='my-4'></hr>" + "<br>" +  "Size:" + " " + $("#size option:selected").text() + "<br>" + "Toppings:" + pizza.toppings.join("") + "<br>"  + "</ul>" + "<br>" + "Total cost:" + "$" + pizza.cost) 
-    
-    if (pizza.toppings.includes("<li>pepperoni --$2")){
-        $(".pizza-crust").addClass("pizza-crust2")
-    }
-    $("#output").show();
-    $(".pizza-crust").show()
-    $("#container-show").show()
-});
+        });
+
+
+        console.log(pizza.toppings)
+
+        pizza.size = parseInt($("#size").val())
+
+        console.log(pizza.size)
+        console.log(pizza.price(pizza))
+        console.log(pizza)
+        $("#output").html("<h4>Your order:</h4>" + "<hr class='my-4'></hr>" + "<br>" + "Size:" + " " + $("#size option:selected").text() + "<br>" + "Toppings:" + pizza.toppings.join("") + "<br>" + "</ul>" + "<br>" + "Total cost:" + "$" + pizza.cost)
+
+        if (pizza.toppings.includes("<li>pepperoni --$2")) {
+            $(".pizza-crust").addClass("pizza-crust2")
+        }
+        if (pizza.toppings.includes("<li>pepperoni --$2" && "<li>onions --$2")) {
+            $(".pizza-crust").addClass("pizza-crust3")
+        }
+
+        $("#output").show();
+        $(".pizza-crust").show()
+        $("#container-show").show()
+    });
 });
